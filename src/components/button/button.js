d53412e60f72a2b37e36ee79deb81f3edf412727
unknown <aliinoccent786@gmail.com>
2024-09-01 03:56:11 -0700

    add button and slider

--- src/components/button/button.js
+++ src/components/button/button.js
@@ -1,8 +1,8 @@
 import './button.css'
 const Btn=()=>{
     return(
-        <div className="container d-flex justify-content-center mt-5 ">
-            <button className="btn" style={{}}>Have work for us</button>
+        <div className="container d-flex justify-content-center " style={{margin:'200px 100px'}}>
+            <button className="btn " style={{height:'40px',width:'340px'}}>Have work for us</button>
         </div>   
     )
 }
--- src/components/slider/slider.js
+++ src/components/slider/slider.js
@@ -3,7 +3,16 @@ import home from '../../img/home.png';
 
 const Slider = () => {
   return (
-    <></>
+    <div className='container bg-white d-flex justify-content-around'>
+      <img src='https://ivkovic.me/themes/min/static/media/partner02.d8f2a498.svg'></img>
+      <img src='https://ivkovic.me/themes/min/static/media/partner05.d39e28a3.svg'></img>
+      <img src='https://ivkovic.me/themes/min/static/media/partner06.a0c63ad5.svg'></img>
+      <img src='https://ivkovic.me/themes/min/static/media/partner03.0d091731.svg'></img>
+      <img src='https://ivkovic.me/themes/min/static/media/partner04.81292e3c.svg'></img>
+      <img src='https://ivkovic.me/themes/min/static/media/partner02.d8f2a498.svg'></img>
+      <img src='https://ivkovic.me/themes/min/static/media/partner01.1f6de9f0.svg'></img>
+
+    </div>
   );
 };
 
